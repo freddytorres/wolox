e709f1b165abcb7f8d5807528eaff6423a6a7c1a
freddyrt17 <freddyrt17@hotmail.com>
2020-08-25 16:45:01 -0500

    commit 1

---
+++ src/main/java/com/wolox/WoloxApplication.java
@@ -0,0 +1,13 @@
+package com.wolox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoloxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoloxApplication.class, args);
+	}
+
+}
